0728012b29b003d2fac351aebdd4318a761af50f
padinag <adina@mini-box.com>
2016-02-10 13:49:01 +0200

    for-a-greater-grade

--- workout-1-sol/code.js
+++ workout-1-sol/code.js
@@ -3,25 +3,24 @@ var content;
 function createCards() {
     console.log("Function createCards executed");
     content = document.getElementById('content');
-for (var i = 0; i < cardsData.length; i++){
+    for (var i = 0; i < cardsData.length; i++){
         var singleCard = document.createElement('div');
-            singleCard.className = "cardStyle";
+        singleCard.className = "cardStyle";
         var imageElem = document.createElement('div');
-            imageElem.className = "image" 
+        imageElem.className = "image" 
         var titleElem = document.createElement('div');
-            titleElem.textContent = cardsData[i].title;
-            titleElem.className = "title" 
-            //console.log(cardsData[i].title);
-            //console.log(titleElem);    
+        titleElem.textContent = cardsData[i].title;
+        titleElem.className = "title" 
+        //console.log(cardsData[i].title);
+        //console.log(titleElem);    
         var descriptionElem = document.createElement('div');
-            descriptionElem.textContent = cardsData[i].description;
-            descriptionElem.className = "description";
-            //console.log(cardsData[i].description);
-            //console.log(descriptionElem);
+        descriptionElem.textContent = cardsData[i].description;
+        descriptionElem.className = "description";
+        //console.log(cardsData[i].description);
+        //console.log(descriptionElem);
         var linkElem = document.createElement('button');
         linkElem.textContent = "OPEN";
         linkElem.adinaCardDataIndex = i;
-        
         linkElem.onclick = function() { 
             console.log(cardsData[this.adinaCardDataIndex].url);
             window.open(cardsData[this.adinaCardDataIndex].url); 
@@ -32,7 +31,7 @@ for (var i = 0; i < cardsData.length; i++){
         singleCard.appendChild(descriptionElem);
         singleCard.appendChild(linkElem);
      
-    content.appendChild(singleCard);
+        content.appendChild(singleCard);
     }   
 }
 
