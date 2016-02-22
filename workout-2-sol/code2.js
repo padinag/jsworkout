f7ca6bc850fa4bed8fbcecdb81c0eafba646af53
padinag <adina@mini-box.com>
2016-02-22 13:15:58 +0200

    started2

--- workout-2-sol/code2.js
+++ workout-2-sol/code2.js
@@ -1,2 +1,16 @@
 
+var request = new XMLHttpRequest();
+request.open('GET', 'https://private-anon-2ee957f6e-rainmachine.apiary-mock.com/api/4/provision');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+      console.log('Status:', this.status);
+      console.log('Headers:', this.getAllResponseHeaders());
+      console.log('Body:', this.responseText);
+      var elem = document.getElementById('responses');
+      elem.textContent = this.responseText;
+  }
+};
+
+request.send();
 
