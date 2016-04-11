55a4ab85e60fe6c3738cc4453efdce5d10ed8f36
padinag <adina@mini-box.com>
2016-04-11 14:47:12 +0300

    iasi location changed

--- workout-3-sol/code3.js
+++ workout-3-sol/code3.js
@@ -1,13 +1,9 @@
 var map;
 function myMap() {
    map = new google.maps.Map(document.getElementByID('map'),{
-       center: {lat: 47.17, lng: 27.57},
+       center: {lat: 47.1585, lng: 27.6014},
        zoom: 8
    }
     );
-<<<<<<< HEAD
 }
 window.onload = myMap();
-=======
-}
->>>>>>> origin/master
