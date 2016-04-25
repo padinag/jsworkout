e74211f16ed176bc3be86cc6a2c8ddb08fa501e0
padinag <adina@mini-box.com>
2016-04-25 13:51:36 +0300

    map-loaded

--- workout-3-sol/code3.js
+++ workout-3-sol/code3.js
@@ -1,9 +1,11 @@
 var map;
 function myMap() {
-   map = new google.maps.Map(document.getElementByID('map'),{
+    mapElem = document.getElementById('map');
+    map = new google.maps.Map(mapElem, {
        center: {lat: 47.156, lng: 27.516},
        zoom: 8
-   }
-    );
+    });
 }
-window.onload = myMap();
+
+window.onload = myMap;
+
